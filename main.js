a9f9e991bbb48bef80d3288f4f7e46b8e9ae30ec
Leila <yasminekaltouma@gmail.com>
2023-03-18 22:47:02 +0000

    second commit

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
-const puppeteer = require('puppeteer')
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+import { createObjectCsvWriter } from "csv-writer";
+import { Octokit } from "@octokit/core";
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
-const csvWriter = createCsvWriter({
-    path: 'cities.csv',
-    header: [
-        {id: 'City', title: 'city'},
-        {id: 'temperature', title: 'temperature'},
-        {id: 'timeStamp', title: 'Timestamp'}
-    ]
+const csvWriter = createObjectCsvWriter({
+  path: 'cities.csv',
+  header: [
+    { id: 'City', title: 'city' },
+    { id: 'temperature', title: 'temperature' },
+    { id: 'timeStamp', title: 'timestamp' }
+  ]
 });
 
+const octokit = new Octokit({
+  auth: process.env.SECRET_TOKEN
+});
 
-async function run () {
-    const browser = await puppeteer.launch({
-        headless:true
-    });
-    const page = await browser.newPage();
-    await page.goto('https://github.com/leilakaltouma/evolution-over-time/commits/main/cities.json');
-    const links = await page.evaluate(() => {
-        const ref = Array.from(document.querySelectorAll(".TimelineItem-body .BtnGroup a:first-child")).map(x => x.getAttribute('href').replace('/blob/','/'))
-        const timeStamp = Array.from(document.querySelectorAll("relative-time")).map(x => x.getAttribute('title'))
-    return  {ref,timeStamp}
-})
+const fileName = 'cities.json';
+const commits = [];
 
-    let cities = []
+async function fetchCommitData(commit) {
+  const response = await fetch(commit);
+  const data = await response.json();
 
-    for(i=0; i < links.ref.length; i++) {
-        await page.goto(`https://raw.githubusercontent.com/${links.ref[i]}`)
-        let data = await page.evaluate(()=> {
-           return  document.querySelector('pre').innerText
-        })
-
-        data = JSON.parse(data)
-        data.forEach((x) => x.timeStamp = links.timeStamp[i])
-        cities.push(data)
-    }
-    csvWriter.writeRecords(cities.flat())
-    .then(() => {
-        console.log('...Done');
-    });
-
-    browser.close();
+  return data;
 }
-run();
+
+async function main() {
+  try {
+    const commitList = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+      owner: 'leilakaltouma',
+      repo: 'evolution-over-time',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+
+    for (const commit of commitList.data) {
+      const data = await fetchCommitData(commit.url);
+
+      if (data.files[0].filename === fileName) {
+        commits.push({'fileUrl': data.files[0].raw_url, 'date': data.commit.author.date});
+      }
+    }
+
+    const cities = [];
+
+    for (const commit of commits) {
+      const data = await fetchCommitData(commit.fileUrl);
+      data.forEach((city) => city.timeStamp = commit.date)
+      cities.push(data);
+    }
+
+
+    await csvWriter.writeRecords(cities.flat());
+    console.log('...Done');
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
